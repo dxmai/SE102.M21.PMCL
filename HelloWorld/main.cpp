b6c2d60d2e8fceff447af07f7176a67c955fdaff
dxmai <19521820@gm.uit.edu.vn>
2022-04-02 17:50:38 +0700

    edit Hello World

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+void main() {
+	cout << "Hello World!" << endl;
+	cout << "Add my name, get started with git" << endl;
+	cout << "Manage git repo" << endl; 
+}
